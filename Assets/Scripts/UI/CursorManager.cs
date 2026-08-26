c3dc7a811c3961d4cd33abeec6428351207cb5e4
Juan Martinez <jjmcarrascosa@gmail.com>
2026-08-26 18:56:37 +0100

    Cursor fixes

--- Assets/Scripts/UI/CursorManager.cs
+++ Assets/Scripts/UI/CursorManager.cs
@@ -10,7 +10,11 @@ public class CursorManager : MonoBehaviour
     public Texture2D defaultCursor;
     public Texture2D waitingCursor;
     public Texture2D disableCursor;
-    public Vector2 hotSpot = Vector2.zero;
+    public Vector2 clickableHotSpot = Vector2.zero;
+    public Vector2 draggableHotSpot = Vector2.zero;
+    public Vector2 defaultHotSpot = Vector2.zero;
+    public Vector2 waitingHotSpot = Vector2.zero;
+    public Vector2 disableHotSpot = Vector2.zero;
 
     void Awake()
     {
@@ -25,31 +29,31 @@ public class CursorManager : MonoBehaviour
     public void SetDraggableCursor()
     {
         if (draggableCursor != null)
-            Cursor.SetCursor(draggableCursor, hotSpot, CursorMode.Auto);
+            Cursor.SetCursor(draggableCursor, draggableHotSpot, CursorMode.Auto);
     }
 
     public void SetClickableCursor()
     {
         if (clickableCursor != null)
-            Cursor.SetCursor(clickableCursor, hotSpot, CursorMode.Auto);
+            Cursor.SetCursor(clickableCursor, clickableHotSpot, CursorMode.Auto);
     }
 
     public void SetDisableCursor()
     {
         if (disableCursor != null)
-            Cursor.SetCursor(disableCursor, hotSpot, CursorMode.Auto);
+            Cursor.SetCursor(disableCursor, disableHotSpot, CursorMode.Auto);
     }
 
     public void SetWaitingCursor()
     {
         if (waitingCursor != null)
-            Cursor.SetCursor(waitingCursor, hotSpot, CursorMode.Auto);
+            Cursor.SetCursor(waitingCursor, waitingHotSpot, CursorMode.Auto);
     }
 
     public void SetDefaultCursor()
     {
         if (defaultCursor != null)
-            Cursor.SetCursor(defaultCursor, hotSpot, CursorMode.Auto);
+            Cursor.SetCursor(defaultCursor, defaultHotSpot, CursorMode.Auto);
         else
             Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
     }
